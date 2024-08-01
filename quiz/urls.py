92d2960194a2b3c2dd6349d62fecd19846ea9851
MarufSwe <khanmaruf.swe@gmail.com>
2024-08-01 12:26:57 +0600

    quiz and result done with time

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  # Home page
     path('quiz/<str:topic_name>/', views.quiz_view, name='quiz-view'),
+    path('quiz-result/<str:topic_name>/', views.quiz_result, name='quiz-result'),
+
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Topic, Question
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Topic, Question, Topic, Question
+from django.views.decorators.csrf import csrf_protect
+from datetime import timedelta
 
 
 def dashboard(request):
@@ -10,9 +12,34 @@ def quiz_view(request, topic_name):
     topic = get_object_or_404(Topic, name=topic_name)
     questions = Question.objects.filter(topic=topic).order_by('?')[:10]  # Shuffle and get 10 questions
 
-    print("questions--------: ", questions)
     context = {
         'topic': topic,
         'questions': questions,
     }
     return render(request, 'quiz_page.html', context)
+
+
+def quiz_result(request, topic_name):
+    if request.method == 'POST':
+        topic = get_object_or_404(Topic, name=topic_name)
+        questions = Question.objects.filter(topic=topic)
+        score = 0
+        total_questions = questions.count()
+
+        for question in questions:
+            selected_answer = request.POST.get(f'question{question.id}')
+            if selected_answer == question.correct_answer:
+                score += 1
+
+        # Calculate time taken for the quiz
+        time_taken_seconds = int(request.POST.get('time_taken', '0'))
+        time_taken = timedelta(seconds=time_taken_seconds)
+
+        return render(request, 'quiz_result.html', {
+            'topic': topic,
+            'score': score,
+            'total_questions': total_questions,
+            'time_taken': time_taken
+        })
+    else:
+        return redirect('dashboard')
