b9a561c7c67fb819a12373a4f54039817b95271a
MarufSwe <khanmaruf.swe@gmail.com>
2024-08-01 18:05:51 +0600

    quiz is done

--- quiz/views.py
+++ quiz/views.py
@@ -22,6 +22,32 @@ def quiz_view(request, topic_name):
     return response
 
 
+# def quiz_result(request, topic_name):
+#     if request.method == 'POST':
+#         topic = get_object_or_404(Topic, name=topic_name)
+#         questions = Question.objects.filter(topic=topic)
+#         score = 0
+#         total_questions = questions.count()
+#
+#         for question in questions:
+#             selected_answer = request.POST.get(f'question{question.id}')
+#             if selected_answer == question.correct_answer:
+#                 score += 1
+#
+#         # Calculate time taken for the quiz
+#         time_taken_seconds = int(request.POST.get('time_taken', '0'))
+#         time_taken = timedelta(seconds=time_taken_seconds)
+#
+#         return render(request, 'quiz_result.html', {
+#             'topic': topic,
+#             'score': score,
+#             'total_questions': total_questions,
+#             'time_taken': time_taken
+#         })
+#     else:
+#         return redirect('dashboard')
+
+
 def quiz_result(request, topic_name):
     if request.method == 'POST':
         topic = get_object_or_404(Topic, name=topic_name)
@@ -29,10 +55,19 @@ def quiz_result(request, topic_name):
         score = 0
         total_questions = questions.count()
 
+        question_details = []
         for question in questions:
             selected_answer = request.POST.get(f'question{question.id}')
-            if selected_answer == question.correct_answer:
+            is_correct = selected_answer == question.correct_answer
+            if is_correct:
                 score += 1
+
+            question_details.append({
+                'question': question,
+                'selected_answer': selected_answer,
+                'correct_answer': question.correct_answer,
+                'is_correct': is_correct
+            })
 
         # Calculate time taken for the quiz
         time_taken_seconds = int(request.POST.get('time_taken', '0'))
@@ -42,7 +77,8 @@ def quiz_result(request, topic_name):
             'topic': topic,
             'score': score,
             'total_questions': total_questions,
-            'time_taken': time_taken
+            'time_taken': time_taken,
+            'question_details': question_details
         })
     else:
         return redirect('dashboard')
